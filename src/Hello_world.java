ad4da290468fd3d321d940f3a1cadb7f812b5a2c
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-21 21:50:06 -0600

    Hello world y comentarios en java

---
+++ src/Hello_world.java
@@ -0,0 +1,16 @@
+public class Hello_world
+{
+    /*
+    Al escribir psvm, automaticamente aparece la clase,
+    de momento no afecta donde vayan los corchetes
+    public static void main(String[] args) {
+
+    }
+    * */
+
+    public static void main (String args[])
+    {
+        System.out.println("HELLO WORLD IN JAVA");
+    }
+
+}
